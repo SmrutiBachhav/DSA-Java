87cac9ac5c3e68594893bb5cfee9489cf1fe80f0
SmrutiBachhav <smrutib2006@gmail.com>
2024-08-02 19:03:10 +0530

    singlyLinkedList q1  hackerrank

---
+++ LinkedLists/SinglyLinkedList_1.java
@@ -0,0 +1,9 @@
+
+    // static void printLinkedList(SinglyLinkedListNode head) {
+    //     SinglyLinkedListNode current=head;
+    //     while(current!=null){
+    //         System.out.println(current.data);
+    //         current=current.next;        
+    //     }
+    // }
+
