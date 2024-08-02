758f2f638f2cf3dcaac6fb748766292d6d4e55cb
SmrutiBachhav <smrutib2006@gmail.com>
2024-08-02 19:33:12 +0530

    SinglyLinkedList q2 hackerrank

---
+++ LinkedLists/SinglyLinkedList_2.java
@@ -0,0 +1,21 @@
+
+    // static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        
+        
+        
+    //     SinglyLinkedListNode newNode=new SinglyLinkedListNode(data);
+        
+    //     SinglyLinkedListNode current=head;
+        
+    //     if(head==null){
+    //         head=newNode;
+    //         return head;
+    //     }
+    //     while(current.next!=null){
+    //         current=current.next;
+            
+    //     }
+    //     current.next=newNode;
+    //     return head;
+        
+    // }
