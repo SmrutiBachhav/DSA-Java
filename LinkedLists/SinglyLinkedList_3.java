2bd8d4d0ce9b5b6676a4d4989e1c09475b9e8acf
SmrutiBachhav <smrutib2006@gmail.com>
2024-08-03 20:28:32 +0530

    inserting node at given position

---
+++ LinkedLists/SinglyLinkedList_3.java
@@ -0,0 +1,17 @@
+// SinglyLinkedListNode newNode=new SinglyLinkedListNode(data);
+//     SinglyLinkedListNode current=head;
+//     if(position==0){
+//         newNode.next=head;
+//         return newNode;
+//     }
+//     int index=0;    //index is starting from 0 to keep track
+//     while(index<position-1){    
+//         //to traverse till position-1 one node before the position we have to add node
+//         current=current.next;
+//         index++;
+//     }
+//     newNode.next=current.next;
+//     current.next=newNode;
+    
+//     return head;
+
